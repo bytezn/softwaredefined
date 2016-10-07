c6f4100152f640ca2bf0cf71ad0b7d3548fc604f
bytezn <lawrancer@rewardsco.co.za>
2016-10-08 00:27:45 +0200

    1

--- fsdswebvltplcmultibkups2sbaldfs-gthub/webfiles/webfile.ps1
+++ fsdswebvltplcmultibkups2sbaldfs-gthub/webfiles/webfile.ps1
@@ -3,5 +3,4 @@ $targetDirectory = "c:\scratch"
 $webdownloadlocation = "https://raw.githubusercontent.com/bytezn/softwaredefined/master/fsdswebvltplcmultibkups2sbaldfs-gthub/webfiles.zip"
 New-Item -ItemType Directory -Force -Path $targetDirectory
 Invoke-WebRequest -Uri $webdownloadlocation -OutFile $targetDirectory\webfiles.zip
-Expand-Archive -Path $targetDirectory\webfiles.zip -DestinationPath C:\scratch\zip
-
+Expand-Archive -Path $targetDirectory\webfiles.zip -DestinationPath $targetDirectory
